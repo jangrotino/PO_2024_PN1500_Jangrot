33a02688f9b2aa0a4a73d25ca49838b9382acbf7
Igor Jangrot <igorjangrot@student.agh.edu.pl>
2024-10-16 00:37:22 +0200

    implementacja klasy world

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,4 +1,31 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.MoveDirection;
+
 public class World {
+    public static void main(String[] args) {
+        System.out.println("system wystartował");
+        run(OptionParser.parser(args));
+        System.out.println("system zakończył działanie");
+    }
+
+    static void run(MoveDirection[] args){
+        System.out.println("Start");
+        for(MoveDirection argument : args){
+            switch(argument){
+                case LEFT:
+                    System.out.println("Zwierzak idzie w lewo");
+                    break;
+                case RIGHT:
+                    System.out.println("Zwierzak skręca w prawo");
+                    break;
+                case FORWARD:
+                    System.out.println("Zwierzak idzie do przodu");
+                    break;
+                case BACKWARD:
+                    System.out.println("Zwierzak idzie do tyłu");
+                    break;
+            }
+        }
+    }
 }
