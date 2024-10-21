be4ebd0b1177ece0ee293d6accaac1890190eca8
Igor Jangrot <igorjangrot@student.agh.edu.pl>
2024-10-21 16:01:01 +0200

    implementacja klasy world

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,12 +1,18 @@
 package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
 
 public class World {
     public static void main(String[] args) {
         System.out.println("system wystartował");
         run(OptionParser.parser(args));
         System.out.println("system zakończył działanie");
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
     }
 
     static void run(MoveDirection[] args){
